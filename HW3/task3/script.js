53e40076a2e7959bdfe77a8d4cb19943b76f4377
GenKozh <gen.kozh@gmail.com>
2024-11-12 13:29:14 +0200

    hw3 fix

--- HW3/task3/script.js
+++ HW3/task3/script.js
@@ -2,17 +2,24 @@
 const button = document.getElementById('myButton');
 const message = document.getElementById('message');
 
+function addMessage(text) {
+  const newMessage = document.createElement('p');
+  newMessage.textContent = text;
+  // Додаємо нове повідомлення в початок контейнера
+  messagesContainer.prepend(newMessage);
+}
+
 // Функція, яка викликається при натисканні кнопки
 button.addEventListener('click', () => {
-  message.textContent = "I was pressed!";
+  addMessage("I was pressed!");
 });
 
 // Функція, яка викликається при наведенні миші на кнопку
 button.addEventListener('mouseover', () => {
-  message.textContent = "Mouse on me!";
+  addMessage("Mouse on me!");
 });
 
 // Функція, яка викликається при відведенні курсора миші з кнопки
 button.addEventListener('mouseout', () => {
-  message.textContent = "Mouse is not on me!";
+  addMessage("Mouse is not on me!");
 });
